3e87a68b12f0b9a11a8f67231712673c6fed3569
DearFishi <89983913+DearFishi@users.noreply.github.com>
2026-04-28 22:01:37 +0800

    [misc]: create kunlun stable image to vllm 0.13.0 and fix vllm import bug (#7)

--- verl/workers/rollout/vllm_rollout/vllm_async_server.py
+++ verl/workers/rollout/vllm_rollout/vllm_async_server.py
@@ -32,21 +32,6 @@ from vllm.inputs import TokensPrompt
 from vllm.lora.request import LoRARequest
 from vllm.outputs import RequestOutput
 from vllm.usage.usage_lib import UsageContext
-
-# _VLLM_VERSION = version.parse(vllm.__version__)
-# if _VLLM_VERSION > version.parse("0.11.0"):
-#     from vllm.utils.argparse_utils import FlexibleArgumentParser
-#     if _VLLM_VERSION == version.parse("0.12.0"):
-#         from vllm.entrypoints.harmony_utils import get_encoding
-#     elif _VLLM_VERSION >= version.parse("0.13.0"):
-#         from vllm.entrypoints.openai.parser.harmony_utils import get_encoding
-#     else:
-#         get_encoding = None
-#     if get_encoding is not None and os.getenv("VERL_USE_GPT_OSS", "0") == "1":
-#         get_encoding()
-# else:
-#     from vllm.utils import FlexibleArgumentParser
-from vllm.utils import FlexibleArgumentParser
 from vllm.v1.engine.async_llm import AsyncLLM
 
 from verl.utils.config import omega_conf_to_dataclass
